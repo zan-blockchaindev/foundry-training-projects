d6c5eb542d96278f539525c5a5e00a154a7d2635
zan-blockchaindev <matiznbstudio@gmail.com>
2024-09-07 15:48:45 +0100

    hello foundry project

---
+++ hello_foundry/src/HelloFoundry.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract HelloFoundry {
+    function hello() public pure returns (string memory) {
+        return "Hello Foundry!";
+    }
+}
